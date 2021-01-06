b8c7512fcafe2e092ecc5cc660966e9ce043aa7c
Luiz <luiz.reis@edu.pucrs.br>
2021-01-06 19:35:56 -0300

    Excluindo file working 100% :fire:

--- excluindo.py
+++ excluindo.py
@@ -13,20 +13,16 @@ LinesTudo = Tudo.readlines()
 LinesFolha = Folha.readlines() 
 LinesEstadao = Estadao.readlines() 
 
-# tudo = 932
-# Folha = 312
-# Estadão = 491
-# --- 803
-for root, dirs, files in os.walk('notícias/resultados - filtro - automático'):
+for root, dirs, files in os.walk('notícias/resultados-filtro-automatico'):
     for file in files:
-        for line in LinesEstadao:
+        for line in LinesFolha:
             filename, extension = os.path.splitext(file)            
             size = len(root.split('/'))
             rootbegin = root.split('/')[2:size]
             ano = rootbegin[0]
             jornal = rootbegin[1]
             keyword = rootbegin[2]  #.split('-')[1]
-            if('Folha' in root): #Estadão ou Folha
+            if('Estadão' in root): #Estadão ou Folha
                 count+=1
                 continue
             try:
@@ -37,5 +33,10 @@ for root, dirs, files in os.walk('notícias/resultados - filtro - automático'):
             if(f'{categoria}\n' == line and root!=Oldroot):
                 Oldroot = root
                 print(f'{num}! - {root}')
+                for file in files:
+                    os.remove(f'{root}/{file}')
+                try:
+                    os.rmdir(root)
+                except:
+                    pass
                 num+=1
-print(f'Count: {count}')
